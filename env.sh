c707a156abbd801cbda5738f8dc11af9d660e344
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-08-01 15:44:48 +0900

    Fix files.

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-GOROOT=/home/isucon/.local/go
-GOPATH=/home/isucon/private_isu/webapp/golang
-PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:$GOROOT/bin:${GOPATH//://bin:}/bin:/home/isucon/.local/scala/bin:/usr/bin/:/bin/:$PATH
-ISUCONP_DB_NAME=isuconp
+export GOROOT=/home/isucon/.local/go
+export GOPATH=/home/isucon/private_isu/webapp/golang
+export PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:$GOROOT/bin:${GOPATH//://bin:}/bin:/home/isucon/.local/scala/bin:/usr/bin/:/bin/:$PATH
+export ISUCONP_DB_NAME=isuconp
